fb228d7a80b87c85783fbbf421bd7c2c88fed319
apoorvkhare1791 <apoorvkhare1791@outlook.com>
2023-07-16 15:14:51 +0530

    message done

---
+++ pulljob.groovy
@@ -0,0 +1,40 @@
+job('Git_Pull'){
+
+  description ("This job is for pull the code from jenkins")
+
+   scm {
+    github("devopsly12/task6-deploy", "main") 
+
+  }
+   triggers {
+      scm("* * * * *")
+  }
+  steps {
+  shell('sh cp -rf * ./ ')
+  }
+
+  job("2 Check And Deploy") {
+  description("This job is for deploying web server")
+  
+  triggers {
+    upstream("Git_Pull")
+  }
+
+
+  steps {
+    shell(sh echo 'deploy the code')
+
+  }
+}
+buildPipelineView("K8s Deploy WebApp") {
+	  title("automating CI/CD pipeline")
+	  description("This is build pipeline view for task 6")
+	  selectedJob("Git_Pull")
+	  alwaysAllowManualTrigger(true)
+	  refreshFrequency(33)
+	  displayedBuilds(1)
+	  showPipelineParameters(true)
+}
+
+
+}
